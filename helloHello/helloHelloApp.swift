23f095a78d9f3b30f94bea311990437c27da872d
Brandon Biggs <bbiggs2425@icloud.com>
2026-06-26 11:35:24 -0700

    Initial clean Xcode project

---
+++ helloHello/helloHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloHelloApp.swift
+//  helloHello
+//
+//  Created by Brandon Biggs on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct helloHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
